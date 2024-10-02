3d27de36b1f7f40c0144ed58f70fb66fd37b004e
NekrodNIK <60639354+nekrodnik@users.noreply.github.com>
2024-10-02 18:22:02 +0700

    fix 1.py

--- 1.py
+++ 1.py
@@ -1,9 +1,8 @@
 def int_bit_count(n: int) -> int:
     count = 0
-    while n > 0:
-        if (n & 1) == 1:
-            count += 1
 
+    while n > 0:
+        count += (n & 1)
         n >>= 1
 
     return count
