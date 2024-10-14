3af75248a6a22aa1fe320d1462d5f589f93b9041
NekrodNIK <60639354+nekrodnik@users.noreply.github.com>
2024-10-14 16:39:35 +0700

    ruff format

--- tests/test_task4.py
+++ tests/test_task4.py
@@ -1,14 +1,18 @@
 from src.task4 import reverse_dict
 
+
 def test_example():
     request = {"Ivanov": 97832, "Petrov": 55521, "Kuznecov": 97832}
     expected = {97832: ("Ivanov", "Kuznecov"), 55521: "Petrov"}
 
     assert reverse_dict(request) == expected
 
+
 def test_general():
     request = {n: n % 2 for n in range(1000)}
-    expected = {1: tuple(n for n in range(1000) if n % 2 == 1),
-                0: tuple(n for n in range(1000) if n % 2 == 0)}
+    expected = {
+        1: tuple(n for n in range(1000) if n % 2 == 1),
+        0: tuple(n for n in range(1000) if n % 2 == 0),
+    }
 
-    assert reverse_dict(request) == expected;
+    assert reverse_dict(request) == expected
