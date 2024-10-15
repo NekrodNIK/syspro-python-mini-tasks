eddc1721119095cf2fdedb3960a221be89c8c2bb
NekrodNIK <60639354+nekrodnik@users.noreply.github.com>
2024-10-15 08:51:38 +0700

    fix

--- src/task4.py
+++ src/task4.py
@@ -5,7 +5,7 @@ def reverse_dict(src: dict) -> dict:
         if new.get(value) is None:
             new[value] = key
         else:
-            if type(new.get(value)) == tuple:
+            if isinstance(new.get(value), tuple):
                 new[value] = (*new[value], key)
             else:
                 new[value] = (new[value], key)
