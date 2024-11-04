763c441b93a0bf51a59ec9a08cff34b8ab9c1c17
NekrodNIK <60639354+nekrodnik@users.noreply.github.com>
2024-11-04 14:09:21 +0700

    fix task7.py tests

--- tests/test_task7.py
+++ tests/test_task7.py
@@ -2,41 +2,56 @@ from src.task7 import deprecated
 from pytest import CaptureFixture
 
 
-sum_1 = deprecated(sum)
-sum_2 = deprecated(since="1.0.0")(sum)
-sum_3 = deprecated(will_be_removed="2.0.0")(sum)
-sum_4 = deprecated(since="1.0.0", will_be_removed="2.0.0")(sum)
+@deprecated
+def func1():
+    pass
+
+
+@deprecated(since="1.0.0")
+def func2():
+    pass
+
+
+@deprecated(will_be_removed="2.0.0")
+def func3():
+    pass
+
+
+@deprecated(since="1.0.0", will_be_removed="2.0.0")
+def func4():
+    pass
+
 
 expected_1 = (
-    f"Warning: function {sum.__name__} is deprecated. "
+    f"Warning: function {func1.__name__} is deprecated. "
     "It will be removed in future versions.\n"
 )
 
 expected_2 = (
-    f"Warning: function {sum.__name__} is deprecated since version 1.0.0. "
+    f"Warning: function {func2.__name__} is deprecated since version 1.0.0. "
     "It will be removed in future versions.\n"
 )
 
 expected_3 = (
-    f"Warning: function {sum.__name__} is deprecated. "
+    f"Warning: function {func3.__name__} is deprecated. "
     "It will be removed in version 2.0.0.\n"
 )
 
 expected_4 = (
-    f"Warning: function {sum.__name__} is deprecated since version 1.0.0. "
+    f"Warning: function {func4.__name__} is deprecated since version 1.0.0. "
     "It will be removed in version 2.0.0.\n"
 )
 
 
 def test(capsys: CaptureFixture):
-    sum_1((1,))
+    func1()
     assert capsys.readouterr()[0] == expected_1
 
-    sum_2((1,))
+    func2()
     assert capsys.readouterr()[0] == expected_2
 
-    sum_3((1,))
+    func3()
     assert capsys.readouterr()[0] == expected_3
 
-    sum_4((1,))
+    func4()
     assert capsys.readouterr()[0] == expected_4
